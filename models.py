0ba42811fef80d04f5d6bf15a25f22d762149c01
idujin <yujin.gha@gmail.com>
2022-04-10 15:10:45 +0900

    2.Define the Network Architecture is now working.

--- models.py
+++ models.py
@@ -24,6 +24,18 @@ class Net(nn.Module):
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1_drop = nn.Dropout(p=0.1)
+        self.fc2_drop = nn.Dropout(p=0.2)
+        self.fc3_drop = nn.Dropout(p=0.3)
+        self.fc4_drop = nn.Dropout(p=0.4)
+        self.fc5_drop = nn.Dropout(p=0.5)
+        
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 1)
+        self.fc1 = nn.Linear(128 * 27 * 27, 1000)
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc3 = nn.Linear(1000, 136)
         
 
         
@@ -31,7 +43,25 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.fc1_drop(x)
         
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.fc2_drop(x)
+        
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.fc3_drop(x)
+        
+        ## flatten
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.fc4_drop(x)
+        
+        x = F.relu(self.fc2(x))
+        x = self.fc5_drop(x)
+        
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
